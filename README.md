38caefe8f639e4b563ae4f63bae8bb9f7b7c8a49
edz <edz@edzdeMacBook-Pro-3.local>
2018-12-15 16:28:48 +0800

    修改了样式，完善了文档

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-一个简单的移动端键盘
+# 一个简单的移动端数字键盘
+
+![160027](https://user-images.githubusercontent.com/23492006/49925297-6c2a0000-fef3-11e8-807b-acf9e5f8db3e.gif)
+
+
+## 安装
+> npm i mini-number-keyboard --save
+
+## 导入
+```js
+import MiniKeyboard from 'mini-number-keyboard'
+or
+<script src="/node_modules/mini-number-keyboard/dist/keyboard.min.js"></srcipt>
+```
+
+## 使用
+```html
+<div id="container" class="keyboard-container"></div>
+<script>
+    let keyboard = new MiniKeyboard({
+        container: 'container'
+    })
+    keyboard.on('keydown', function(key) {
+        console.log(key)
+    })
+</script>
+```
--- src/core/index.js
+++ src/core/index.js
@@ -1,41 +1,44 @@
 import InitEvent from '../modules/events'
-
+const arrow = require('../static/arrow.png')
 class Keyboard {
     constructor(options) {
         this.options = Object.assign({
             container: ''
         }, options)
         this.container = document.getElementById(this.options.container)
+        this.container.classList.add('keyboard-container')
         this.initDom()
         this.initEvent()
     }
 
     initDom() {
         let domStr = `
-            <div class="left">
-                <div>
-                    <span data-keycode="1">1</span>
-                    <span data-keycode="2">2</span>
-                    <span data-keycode="3">3</span>
+            <div class="keyboard-container">
+                <div class="left">
+                    <div>
+                        <span data-keycode="1">1</span>
+                        <span data-keycode="2">2</span>
+                        <span data-keycode="3">3</span>
+                    </div>
+                    <div>
+                        <span data-keycode="4">4</span>
+                        <span data-keycode="5">5</span>
+                        <span data-keycode="6">6</span>
+                    </div>
+                    <div>
+                        <span data-keycode="7">7</span>
+                        <span data-keycode="8">8</span>
+                        <span data-keycode="9">9</span>
+                    </div>
+                    <div>
+                        <span data-keycode="dot">.</span>
+                        <span data-keycode="0">0</span>
+                        <span data-keycode="del"><img src="${arrow}"></span>
+                    </div>
+                </div><div class="right">
+                    <div class="top" data-keycode="next">下一项</div>
+                    <div class="bottom" data-keycode="confirm">确认</div>
                 </div>
-                <div>
-                    <span data-keycode="4">4</span>
-                    <span data-keycode="5">5</span>
-                    <span data-keycode="6">6</span>
-                </div>
-                <div>
-                    <span data-keycode="7">7</span>
-                    <span data-keycode="8">8</span>
-                    <span data-keycode="9">9</span>
-                </div>
-                <div>
-                    <span data-keycode="dot">.</span>
-                    <span data-keycode="0">0</span>
-                    <span data-keycode="del">删除</span>
-                </div>
-            </div><div class="right">
-                <div class="top" data-keycode="next">下一项</div>
-                <div class="bottom" data-keycode="confirm">确认</div>
             </div>
         </div>
         `
@@ -44,11 +47,14 @@ class Keyboard {
     }
 
     initEvent() {
-        this.container.addEventListener('click', e => {
-            let target = e.target
-            let keyCode = target.getAttribute('data-keycode')
-            if(keyCode) {
-                this.dispatchEvent('keydown', keyCode)
+        const self = this
+        this.container.addEventListener('click', function(e) {
+            for(let target = e.target; target && target != this; target = target.parentNode) {
+                let keyCode = target.getAttribute('data-keycode')
+                if(keyCode) {
+                    self.dispatchEvent('keydown', keyCode)
+                    return
+                }
             }
         })
     }
